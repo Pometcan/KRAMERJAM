9eb6244ceb92fc9139e1263b564eb29c70c20c94
Anıl Uzun <125743440+anil-uzun@users.noreply.github.com>
2025-05-31 17:49:03 +0300

    update

--- scripts/collectible/collectible.gd
+++ scripts/collectible/collectible.gd
@@ -1,12 +1,21 @@
 extends Area2D
-@onready var player: CharacterBody2D = $"."
 
+var i = 0
+var area_names = [""]
+var player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if (player in get_overlapping_bodies()) and Input.is_action_just_pressed("interract"):
+	i = 0
+	for area in get_overlapping_areas():
+		area_names[i] = area.name
+		print(area_names)
+		i += 1
+		#if area.name == "interractArea":
+			#player = area.get_node("./Player")
+			#if "interractArea" in area_names and Input.is_action_pressed("interract"):
+				#player.inventory = ["test"]
 		queue_free()
-		print(get_overlapping_bodies())
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,17 +4,24 @@ const SPEED := 100 * 500
 var direction := Vector2(0.0,0.0)
 var h_axis := 0.0
 var v_axis := 0.0
-var fliph := true
+var fliph := false
 var mouse_pos := position
 var moving_to_mouse := false
 var friction := 0.05
 var in_bounds := true
+var original_scale := scale
+var inventory = []
+
 @onready var player_sprite: AnimatedSprite2D = $playerSprite
+@onready var interract_area: Area2D = $playerSprite/interractArea
 
 func _ready() -> void:
 	pass
 	
 func _physics_process(_delta: float) -> void:
+	###
+	###MOVEMENT
+	###
 	if not moving_to_mouse:
 		velocity = Vector2(0,0)
 	if in_bounds:
@@ -25,8 +32,7 @@ func _physics_process(_delta: float) -> void:
 			velocity = direction * SPEED * _delta
 			moving_to_mouse = true
 		if moving_to_mouse:
-			if (mouse_pos - position).length() < 0.8:
-				velocity = Vector2(0,0)
+			if (mouse_pos - position).length() < SPEED/100:
 				moving_to_mouse = false
 			else:
 				direction = (mouse_pos - position).normalized()
@@ -37,9 +43,15 @@ func _physics_process(_delta: float) -> void:
 		
 		if (h_axis < 0 or velocity.x < 0):
 			player_sprite.play("walk")
+			if fliph:
+				player_sprite.flip_h = true
+				interract_area.position.x = 0
 			fliph = false
 		elif (h_axis > 0 or velocity.x > 0):
 			player_sprite.play("walk")
+			if !fliph:
+				player_sprite.flip_h = false
+				interract_area.position.x = 300
 			fliph = true
 		else: 
 			player_sprite.play("idle")
@@ -55,5 +67,9 @@ func _physics_process(_delta: float) -> void:
 		if Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_up"):
 			velocity = direction * SPEED * _delta
 			moving_to_mouse = false
-		
 	move_and_slide()
+	##
+	## INVENTORY
+	##
+	
+	
