2284a434f96ca24c5c64f99a8b161595c0651c79
Gandalfistemoruq <61329361+ugurdk100@users.noreply.github.com>
2025-06-01 12:41:28 +0300

    Upgrade to menus

--- scenes/levels/main.gd
+++ scenes/levels/main.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@onready var pause_menu: Control = $"Player/Camera2D/Pause Menu"
+@onready var pause_menu: Control = $"Player/Pause Menu"
 var paused = false 
 
 func _ready(): 
--- scenes/levels/pause_menu.gd
+++ scenes/levels/pause_menu.gd
@@ -1,10 +1,14 @@
 extends Control
 
-
-
-
+@onready var main: Node2D = $"../.."
+@onready var pause_menu: Control = $"."
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_dewam_pressed() -> void:
+	main.pauseMenu()
 	pass # Replace with function body.
